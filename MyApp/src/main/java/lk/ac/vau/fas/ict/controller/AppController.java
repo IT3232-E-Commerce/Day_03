f10d8c31c7d01818bbd5185e2fef4467dee70eb9
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2025-03-30 12:53:50 +0530

    Code updated

--- MyApp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
+++ MyApp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -74,5 +76,11 @@ public class AppController {
     		}
     	}
     	return sortedStudents;
+    }
+    
+    @PostMapping("/students")
+    public String createStudent(@RequestBody Student student) {
+        students.add(student);
+        return "Student added successfully!";
     }
 }
