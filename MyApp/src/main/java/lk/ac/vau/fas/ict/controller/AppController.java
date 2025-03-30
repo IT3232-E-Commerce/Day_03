57252191d5c13f5d7a04b4f7432303b8be2f6d21
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2025-03-30 14:04:37 +0530

    Code updated

--- MyApp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
+++ MyApp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -82,5 +83,19 @@ public class AppController {
     public String createStudent(@RequestBody Student student) {
         students.add(student);
         return "Student added successfully!";
-    }
+    }   
+    
+    @PutMapping("/students/{id}")
+    public String updateStudents(@PathVariable("id") String regNo, @RequestBody Student updatedStudent) {
+        for (Student student : students) {
+        	if (student.getRegNo().equals(regNo)) {
+        		student.setName(updatedStudent.getName());
+        		student.setAge(updatedStudent.getAge());
+        		student.setCourse(updatedStudent.getCourse());
+        		student.setGpa(updatedStudent.getGpa());
+        		return "Student updated successfully!";
+        	}
+        }
+        return "Student not found!";
+    } 
 }
